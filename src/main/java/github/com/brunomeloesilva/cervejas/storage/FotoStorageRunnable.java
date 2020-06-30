519e2380f46c9001127a60b49d09734810d5cffc
brunomeloesilva <brunomeloesilva@gmail.com>
2020-06-30 15:29:01 -0400

    14.7. Salvando a imagem temporária

--- src/main/java/github/com/brunomeloesilva/cervejas/storage/FotoStorageRunnable.java
+++ src/main/java/github/com/brunomeloesilva/cervejas/storage/FotoStorageRunnable.java
@@ -20,9 +20,7 @@ public class FotoStorageRunnable implements Runnable {
 	@Override
 	public void run() {
 		
-		this.fotoStorage.salvarTemporariamente(files);
-		
-		String nomeFoto = files[0].getOriginalFilename();
+		String nomeFoto = this.fotoStorage.salvarTemporariamente(files);
 		String contentType = files[0].getContentType();
 		deferredResult.setResult(new FotoDTO(nomeFoto, contentType));
 	}
