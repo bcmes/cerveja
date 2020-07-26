988b3d432fd63b389b061e0ee0ce9aaf79b57bc1
brunomeloesilva <brunomeloesilva@gmail.com>
2020-07-26 14:11:19 -0400

    16.6 Implementando Estados..

---
+++ src/main/java/github/com/brunomeloesilva/cervejas/repository/Estados.java
@@ -0,0 +1,9 @@
+package github.com.brunomeloesilva.cervejas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import github.com.brunomeloesilva.cervejas.model.Estado;
+
+public interface Estados extends JpaRepository<Estado, Long>{
+
+}
